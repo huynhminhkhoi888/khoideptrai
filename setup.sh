aeea93cb186911e03d39f1c387f534521e4a65e1
KhoiHuynh1109 <147224811+huynhminhkhoi888@users.noreply.github.com>
2023-10-23 23:03:25 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+home='/data/data/com.termux/files/'
+curl -s https://huynhminhkhoi.000webhostapp.com/KunTool/hi.txt -o $home/usr/bin/kuntool
+chmod 777 $home/usr/bin/kuntool
+printf '\033[1;32mCài Đặc Thành Công Nhập kuntool Để Vào Tool\n\n'
